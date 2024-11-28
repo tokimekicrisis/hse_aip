57a448b9bbc97b659f5dddd70b5ef1f616d264f5
tokimekicrisis <anna-shershina@yandex.ru>
2024-11-28 18:25:20 +0300

    upd

--- main.py
+++ main.py
@@ -1,34 +1,80 @@
-import sys
 import pygame
-from mathgame import MathField
+from mathgame import MathField, print_ui
 
+
+# GAME STATES: "math1", "math2", "rus1", "rus2", "menu", "gameover"
 
 class Button:
-    def __init__(self, text, w, h):
+    def __init__(self, text, x, y, w, h):
         self.text = text
-        self.center_w, self.center_h = w, h
-        txt = text_font.render(self.text, False, (255, 255, 255))
-        self.text_rect = txt.get_rect(center=(self.center_w, self.center_h))
-        self.text_rect2 = pygame.Rect(0, 0, 180, self.text_rect.height)
-        self.text_rect2.center = (self.center_w, self.center_h)
-        self.left, self.top, self.width, self.height = \
-            self.text_rect2.left, self.text_rect2.top, self.text_rect2.width, self.text_rect2.height
+        self.width, self.height = w, h
+        self.btn = pygame.Rect(0, 0, w, h)
+        self.btn.center = (x, y)
+        self.left, self.top = self.btn.left, self.btn.top
 
-    def hover(self, pos):
-        if self.left < pos[0] < self.left + self.width:
-            if self.top < pos[1] < self.top + self.height:
+    def hover(self, position):
+        if self.left < position[0] < self.left + self.width:
+            if self.top < position[1] < self.top + self.height:
                 return True
         return False
 
     def display(self):
         if self.hover(pos):
-            txt = text_font.render(self.text, False, (0, 255, 255))
-            pygame.draw.rect(screen, (0, 255, 255), self.text_rect2, width=1)
+            color = (255, 255, 0)
         else:
-            txt = text_font.render(self.text, False, (255, 255, 255))
-            pygame.draw.rect(screen, (255, 255, 255), self.text_rect2, width=1)
-        screen.blit(txt, self.text_rect)
+            color = (255, 255, 255)
+        txt = text_font.render(self.text, False, color)
+        pygame.draw.rect(screen, color, self.btn, 1)
+        text_rect = txt.get_rect(center=self.btn.center)
+        screen.blit(txt, text_rect)
 
+
+class Counter:
+    def __init__(self, time):
+        self.time = time
+
+    def go_down(self):
+        self.time -= 1
+
+    def display(self):
+        if self.time > 5:
+            color = (255, 255, 255)
+        else:
+            color = (255, 0, 0)
+        line = number_font.render(str(self.time).ljust(2), False, color)
+        text_rect = line.get_rect(center=(45, 450))
+        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 400, 200, 63))
+        screen.blit(line, text_rect)
+
+def show_menu():
+    screen.fill((0, 0, 0))
+    line = title_font.render("РАЗВИВАШКИ", False, (255, 255, 255))
+    text_rect = line.get_rect(center=(250, 60))
+    screen.blit(line, text_rect)
+    mt1 = Button("test", 250, 250, 100, 100)
+    mt1.display()
+    mt2, ru1, ru2 = 0, 0, 0
+    return mt1, mt2, ru1, ru2
+
+def start_game(state):
+    if state == "math1":
+        field = MathField()
+        field.refresh(screen, text_font)
+        print_ui(screen, text_font, "Счёт: " + str(field.score).zfill(5), 400, 475)
+        print_ui(screen, text_font, "Жизней: " + str(field.lives), 85, 475)
+        return field
+
+def game_over(score):
+    screen.fill((0, 0, 0))
+    line1 = title_font.render("Игра окончена!", False, (255, 255, 255))
+    line2 = text_font.render("Твой счёт: " + str(score), False, (255, 255, 255))
+    menu_btn = Button("Главное меню", 250, 400, 200, 100)
+    menu_btn.display()
+    text_rect1 = line1.get_rect(center=(250, 150))
+    text_rect2 = line2.get_rect(center=(250, 200))
+    screen.blit(line1, text_rect1)
+    screen.blit(line2, text_rect2)
+    return menu_btn
 
 if __name__ == '__main__':
     pygame.init()
@@ -37,33 +83,64 @@ if __name__ == '__main__':
     pygame.display.set_caption('Развивашки')
     clock = pygame.time.Clock()
 
-    number_font = pygame.font.Font("resources/HomeVideo-Regular.otf", 25)
-    text_font = pygame.font.Font("resources/HomeVideo-Regular.otf", 25)
+    text_font = pygame.font.Font("resources/Monocraft.otf", 20)
+    title_font = pygame.font.Font("resources/Monocraft.otf", 40)
+    number_font = pygame.font.Font("resources/Monocraft.otf", 30)
+    pygame.time.set_timer(pygame.USEREVENT, 1000) # таймер для математических игр
 
-    game_state = "math"
-    field = MathField(screen)
+    game_state = "menu"
 
     running = True
     while running:
-        if game_state == "math":
-            field.draw_field(screen)
-            field.draw_numbers(screen, number_font)
-            field.print_question(screen, text_font)
-            field.print_score(screen, text_font)
-            for event in pygame.event.get():
-                pos = pygame.mouse.get_pos()
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        field.select(screen, text_font)
-                    elif event.key == pygame.K_RETURN:
-                        field.refresh(screen)
-                    else:
-                        field.move_player(event.key)
-            if field.answers == field.selected:
-                field.refresh(screen)
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                running = False # если пользователь нажал крестик, игра кончается
+            else:
+                if game_state == "gameover":
+                    menu_return = game_over(score)
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if menu_return.hover(pos):
+                            game_state = "menu"
+                if game_state == "menu":
+                    buttons = show_menu()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if buttons[0].hover(pos):
+                            game_state = "math1"
+                            field = start_game(game_state)
+                            rounds = 0
+                            counter = Counter(30)
+                if game_state == "math1":
+                    counter.display()
+                    field.draw_field(screen)
+                    field.draw_numbers(screen, text_font)
+                    if event.type == pygame.USEREVENT:
+                        counter.go_down()
+                        if counter.time == 0:
+                            if len(field.selected) == 0:
+                                field.lives -= 1
+                                pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 400, 200, 100))
+                                print_ui(screen, text_font, "Жизней: " + str(field.lives), 85, 475)
+                            field.refresh(screen, text_font)
+                            if rounds < 15:
+                                rounds += 1
+                            counter = Counter(30 - rounds // 3 * 5)
+                            field.multiplier = rounds // 3 + 1
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            field.select(screen, text_font)
+                        else:
+                            field.move_player(event.key)
+                    if field.answers == field.selected:
+                        field.refresh(screen, text_font)
+                        if rounds < 15:
+                            rounds += 1
+                        counter = Counter(30 - rounds // 3 * 5)
+                        field.multiplier = rounds // 3 + 1
+                    if field.lives == 0:
+                        game_state = "gameover"
+                        score = field.score
         pygame.display.flip()
-        clock.tick(15)
+    clock.tick(15)
 
 pygame.quit()
--- mathgame.py
+++ mathgame.py
@@ -3,6 +3,16 @@ import pygame
 
 
 def find_correct(q, num):
+    """
+    Определяет, является ли число правильным ответом на данный вопрос.
+
+    Args:
+        q (int): ID вопроса.
+        num (int): Число.
+
+    Returns:
+        bool: True, если является, False, если нет.
+    """
     if q == 0:
         return num % 2 == 0
     if q == 1:
@@ -28,31 +38,76 @@ def find_correct(q, num):
         return num % 4 == 0
 
 
+def print_ui(screen, myfont, text, x, y):
+    line = myfont.render(text, False, (255, 255, 255))
+    text_rect = line.get_rect(center=(x, y))
+    screen.blit(line, text_rect)
+
+
 class MathField:
-    def __init__(self, screen):
+    """
+    Поле для игры №1 (на знание свойств чисел).
+
+    Attributes:
+        numbers (list): Числа на поле.
+        x (int): x-координата позиции игрока.
+        y (int): y-координата позиции игрока.
+        question (int): ID вопроса.
+        answers (set): Множество координат ячеек с числами, удовлетворяющими вопросу.
+        selected (set): Множество координат уже выбранных игроком чисел.
+        lives (int): Количество остающихся жизней.
+        score (int): Счет.
+        multiplier (int): Множитель, на который домножаются очки игрока. Увеличивается с продолжительностью игры.
+    """
+    def __init__(self):
+        """
+        Создает новое поле. Почти все атрибуты нулевые или пустые, жизней 5, множитель равен 1.
+        """
         self.numbers = []
         self.x, self.y = 0, 0
         self.question = 0
         self.answers = set()
         self.selected = set()
+        self.lives = 5
         self.score, self.multiplier = 0, 1
-        self.refresh(screen)
 
     def set_answers(self):
+        """
+        Проходит по всему полю и устанавливает правильные ответы.
+        """
         self.answers = set()
         for i in range(5):
             for j in range(5):
                 if find_correct(self.question, self.numbers[i][j]):
                     self.answers.add((i, j))
 
-    def refresh(self, screen):
+    def refresh(self, screen, myfont):
+        """
+        Обновляет данные (генерирует новые числа и новый вопрос,
+        создает пустое множество выбранных ячеек, устанавливает
+        верные ответы), и выводит новый вопрос.
+        По факту начинает новый раунд.
+
+        Args:
+            screen (pygame.Surface): Экран.
+            myfont (pygame.font.Font): Используемый шрифт.
+        """
         self.numbers = [[randint(1, 100) for i in range(5)] for j in range(5)]
         self.question = randint(0, 9)
         self.selected = set()
         pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, 500, 100))
+        print_ui(screen, myfont, questions[self.question], 250, 60)
         self.set_answers()
 
     def draw_field(self, screen):
+        """
+        Рисует поле на экране. Если ячейка уже была выбрана -
+        она не перерисовывается. Если нет: если игрок
+        сейчас на ней стоит - фон желтый, если нет - нет фона.
+
+        Args:
+            screen (pygame.Surface): Экран.
+        """
         x, y = 125, 125
         for row in range(5):
             for column in range(5):
@@ -70,6 +125,14 @@ class MathField:
             y = 125
 
     def draw_numbers(self, screen, myfont):
+        """
+        Рисует числа на поле. Если игрок стоит на ячейке -
+        текст черный, если нет - текст белый.
+
+        Args:
+            screen (pygame.Surface): Экран.
+            myfont (pygame.font.Font): Используемый шрифт.
+        """
         x, y = 125, 125
         for row in range(5):
             for column in range(5):
@@ -85,6 +148,13 @@ class MathField:
             y = 125
 
     def move_player(self, event):
+        """
+        Смотрит, на какую клавишу нажал игрок. Если клавиша -
+        стрелочка, то двигает игрока соответственно.
+
+        Args:
+            event (pygame.Event): Событие (в данном случае нажатие клавиши).
+        """
         if event == pygame.K_UP:
             if self.y == 0:
                 self.y = 4
@@ -107,31 +177,35 @@ class MathField:
                 self.x -= 1
 
     def select(self, screen, myfont):
+        """
+        Выбор ячейки.
+        Если ячейка есть в множестве правильных ответов - игроку начисляются
+        очки, ячейка отмечается зеленым и добавляется в множество уже выбранных.
+        Если ячейка правильная, но уже была выбрана - ничего не происходит.
+        Если ячейка неправильная - игрок теряет одну жизнь.
+
+        Args:
+            screen (pygame.Surface): Экран.
+            myfont (pygame.font.Font): Используемый шрифт.
+        """
         position = (self.x, self.y)
         print(self.answers, self.selected)
         if position in self.answers:
             if position not in self.selected:
                 print("Правильно!")
-                pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(self.x * 50 + 125 + (1 + self.x) * 2,
+                pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(self.x * 50 + 125 + (1 + self.x) * 2,
                                                                   self.y * 50 + 125 + (1 + self.y) * 2, 46, 46))
                 self.selected.add(position)
-                self.score += 100 * self.multiplier
-                self.print_score(screen, myfont)
+                self.score += 50 * self.multiplier
+                pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(300, 400, 200, 100))
+                print_ui(screen, myfont, "Счёт: " + str(self.score).zfill(5), 400, 475)
             else:
                 print("Уже выбрано!")
         else:
             print("Неправильно!")
-
-    def print_question(self, screen, myfont):
-        line = myfont.render(questions[self.question], False, (255, 255, 255))
-        text_rect = line.get_rect(center=(250, 60))
-        screen.blit(line, text_rect)
-
-    def print_score(self, screen, myfont):
-        pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 400, 500, 100))
-        score = myfont.render("Счёт: " + str(self.score), False, (255, 255, 255))
-        text_rect = score.get_rect(center=(400, 475))
-        screen.blit(score, text_rect)
+            self.lives -= 1
+            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 400, 200, 100))
+            print_ui(screen, myfont, "Жизней: " + str(self.lives), 85, 475)
 
 
 questions = {0: "Выбери все четные числа!",
@@ -143,4 +217,4 @@ questions = {0: "Выбери все четные числа!",
              6: "Выбери все числа-квадраты!",
              7: "Выбери все однозначные числа!",
              8: "Выбери все числа, кратные 3!",
-             9: "Выбери все числа, кратные 4!"}
+             9: "Выбери все числа, кратные 4!"} # словарь вопросов
